76c28a8faa40ff640bde5025adac357fa5ad13d1
もっへもへ <mohemohe@users.noreply.github.com>
2014-12-19 23:54:17 +0900

    うおおおおおおおおおおおおああああああああああああああああああああああああああああああ！！！！！！！
    うおおおおおおおおおおおおあああああああああああああああああああああああああああああああ！！！！！！！！！！！ (ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！)

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -3,6 +3,7 @@ Plugin.create(:butimiri_rantotu) do
 	on_appear do |ms|
 		ms.each do |m|
 			if m.message.to_me?()
+				# うおおおおおおおおおおおおあああああああああああああああああああああああああああああああ！！！！！！！！！！！ (ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！)
 				if m.message.to_s =~ /うおおお|おおおおお|おあああああ|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet?
 					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(40)}", :replyto => m)
 					m.message.favorite(true)
