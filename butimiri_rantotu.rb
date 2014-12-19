a3cbb7219761bfa084208058aa58aab28f32c0a0
karubabu <karubabu8810@gmail.com>
2014-12-19 21:42:42 +0900

    とりあえず動いた

---
+++ butimiri_rantotu.rb
@@ -0,0 +1,13 @@
+Plugin.create(:butimiri_rantotu) do
+	DEFINED_TIME = Time.new.freeze
+	on_appear do |ms|
+		ms.each do |m|
+			if m.message.to_me?()
+				if m.message.to_s =~ /うおおお|おおおおお|おあああああ|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet?
+					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(40)}", :replyto => m)
+					m.message.favorite(true)
+				end
+			end
+		end
+	end
+end
