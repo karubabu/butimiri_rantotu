9257c78f7908182a42c840b2083acc0795fe4867
karubabu <karubabu8810@gmail.com>
2014-12-22 02:21:49 +0900

    elsifの使い方がダメだったので修正

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -7,8 +7,9 @@ Plugin.create(:butimiri_rantotu) do
 					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
 					m.message.favorite(true)
 				elsif m.message.to_s =~ /うおおお|おおおおお|おあああああ|/ and m[:created] > DEFINED_TIME and !m.retweet?
-					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸うるさい" + "！"+ "！" * rand(50)}", :replyto => m)
-					m.message.favorite(true)
+						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸うるさい" + "！"+ "！" * rand(50)}", :replyto => m)
+						m.message.favorite(true)
+				else
 				end
 			end
 		end
