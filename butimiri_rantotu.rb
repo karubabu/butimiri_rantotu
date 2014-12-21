14703b7a0f03368e53847e65a759999520421366
karubabu <karubabu8810@gmail.com>
2014-12-22 01:40:09 +0900

    ブチミリしてない叫ぶだけマンを別個にするあれ

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -3,8 +3,11 @@ Plugin.create(:butimiri_rantotu) do
 	on_appear do |ms|
 		ms.each do |m|
 			if m.message.to_me?()
-				if m.message.to_s =~ /うおおお|おおおおお|おあああああ|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet?
-					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(40)}", :replyto => m)
+				if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet?
+					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
+					m.message.favorite(true)
+				elsif m.message.to_s =~ /うおおお|おおおおお|おあああああ|/ and m[:created] > DEFINED_TIME and !m.retweet?
+					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸うるさい" + "！"+ "！" * rand(50)}", :replyto => m)
 					m.message.favorite(true)
 				end
 			end
