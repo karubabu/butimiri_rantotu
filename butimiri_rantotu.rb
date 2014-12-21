efdedb5590c431dd410d08fc0cbdd90635499680
karubabu <karubabu8810@gmail.com>
2014-12-22 02:24:39 +0900

    まだだめだった

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -3,10 +3,10 @@ Plugin.create(:butimiri_rantotu) do
 	on_appear do |ms|
 		ms.each do |m|
 			if m.message.to_me?()
-				if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet?
+				if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
 					m.message.favorite(true)
-				elsif m.message.to_s =~ /うおおお|おおおおお|おあああああ|/ and m[:created] > DEFINED_TIME and !m.retweet?
+				elsif m.message.to_s =~ /うおおお|おおおおお|おあああああ|/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸うるさい" + "！"+ "！" * rand(50)}", :replyto => m)
 						m.message.favorite(true)
 				else
