955ca2014450cea8c6eea94603ac666a9979ade7
karubabu <karubabu8810@gmail.com>
2014-12-22 18:35:15 +0900

    叫び声側の正規表現をちょっと修正した

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -6,7 +6,7 @@ Plugin.create(:butimiri_rantotu) do
 				if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
 					m.message.favorite(true)
-				elsif m.message.to_s =~ /うおおお|おおおおお|おあああああ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+				elsif m.message.to_s =~ /([うおあ！]){4,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸うるさい" + "！"+ "！" * rand(20)}", :replyto => m)
 					m.message.favorite(true)
 				else
