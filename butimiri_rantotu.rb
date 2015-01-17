c894f171799b46841f32fd71db006f5deeb637af
karubabu <karubabu8810@gmail.com>
2015-01-17 15:04:00 +0900

    ちょっとだけ(´･ω･｀)うるさいを緩和した

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -4,12 +4,12 @@ Plugin.create(:butimiri_rantotu) do
 		ms.each do |m|
 			if m.message.to_me?()
 				if !m.message.from_me?()
-					if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([ｂｕｔｉｍｒ]){4,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([ｂｕｔｉｍｒ]){4,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
 						m.message.favorite(true)
 					elsif m.message.to_s =~ /ｯ！/ and m[:created] > DEFINED_TIME and !m.retweet? then
 
-					elsif m.message.to_s =~ /([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif m.message.to_s =~ /([うおあｕｏａ]){5,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)うるさい" + "！"+ "！" * rand(20)}", :replyto => m)
 						m.message.favorite(true)
 					end
