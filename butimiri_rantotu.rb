d667da12759da9175d18c189db3f01d57df240e8
karubabu <karubabu8810@gmail.com>
2015-05-14 19:44:46 +0900

    なんかした

--- butimiri_rantotu.rb
+++ butimiri_rantotu.rb
@@ -4,12 +4,12 @@ Plugin.create(:butimiri_rantotu) do
 		ms.each do |m|
 			if m.message.to_me?()
 				if !m.message.from_me?()
-					if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|㍉|ﾌﾞﾁ|([ｂｕｔｉｍｒbutimr]){4,}|([ぶちみりブチミリ]){4,}/ and m[:created] > DEFINED_TIME and !m.retweet? and m.message.to_s !~ /[\(（]@karubabu\s?[\)）]/ then
+					if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|㍉|ﾌﾞﾁ|ぶちっ|みりっ|([ぶちみりブチミリ]){4,}/ and m[:created] > DEFINED_TIME and !m.retweet? and m.message.to_s !~ /[\(（]@karubabu\s?[\)）]/ then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)凸" + "！" * rand(50)}", :replyto => m)
 						m.message.favorite(true)
 					elsif m.message.to_s =~ /ｯ！/ and m[:created] > DEFINED_TIME and !m.retweet? then
 
-					elsif m.message.to_s =~ /([うおあｕｏａ]){5,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif m.message.to_s =~ /([うおあｕｏａio]){5,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + "(´･ω･｀)うるさい" + "！"+ "！" * rand(20)}", :replyto => m)
 						m.message.favorite(true)
 					end
